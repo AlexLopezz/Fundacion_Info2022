13a43fe926e33e59a7991bb9ed408c526935b6cc
Alex Lopez <alexxxd23@gmail.com>
2022-08-15 22:49:09 -0300

    agregue html noticia y modifique seccion de noticias.

--- apps/noticias/urls.py
+++ apps/noticias/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import noticias
+from .views import noticias, articulo
 
 urlpatterns = [
-    path('noticias/', noticias, name='noticias'),
+    path('noticias/', noticias.as_view(), name='noticias'),
+    path('articulo/<int:pk>',articulo.as_view(), name= 'articulo'),
 ]
