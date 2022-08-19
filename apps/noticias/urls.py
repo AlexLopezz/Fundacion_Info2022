9a2984fa967c594b57eca1487cbf9b565041e016
Alex <alexxxd23@gmail.com>
2022-08-18 23:50:58 -0300

    funcionalidad

--- apps/noticias/urls.py
+++ apps/noticias/urls.py
@@ -4,5 +4,5 @@ from .views import noticias, articulo, categoria
 urlpatterns = [
     path('noticias/', noticias.as_view(), name='noticias'),
     path('articulo/<int:pk>',articulo.as_view(), name= 'articulo'),
-    path('categoria/<str:cat>', categoria, name ='categoria'),
+    path('categoria/<int:cat>', categoria, name ='categoria'),
 ]
