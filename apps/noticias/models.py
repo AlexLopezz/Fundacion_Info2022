453a5d6cc7f1002aa5686da2b6ecdc9e6dc331c4
JorgeGtt <jorgehumgianetti@gmail.com>
2022-08-18 22:45:16 -0300

    se coloca los acceso a Mision y quienes somos

--- apps/noticias/models.py
+++ apps/noticias/models.py
@@ -6,17 +6,20 @@ class Categoria(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=50, null = True, blank= True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.nombre
+    
+
 
 class Noticia(models.Model):
     titulo = models.CharField(max_length=150)
     autor = models.CharField(max_length=50, null = True, blank=True)
     fechaCreacion = models.DateField(auto_now_add=True)
+    descripciones = models.TextField()
     contenido = models.TextField()
     imagen = models.ImageField(upload_to= 'noticias', null=True, blank = True)
     categoria = models.ForeignKey(Categoria, on_delete= models.CASCADE, null=True, blank= True)
 
-    categoria = models.ForeignKey(Categoria, on_delete= models.CASCADE, null=True)
+    
     def __str__(self) -> str:
         return self.titulo
--- apps/noticias/urls.py
+++ apps/noticias/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import noticias
+from .views import noticias, articulo, categoria, mision, somos
 
 urlpatterns = [
-    path('noticias/', noticias, name='noticias'),
+    path('noticias/', noticias.as_view(), name='noticias'),
+    path('articulo/<int:pk>',articulo.as_view(), name= 'articulo'),
+    path('categoria/<str:cat>', categoria, name ='categoria'),
+    path('mision/', mision, name = 'mision'),
+    path('somos/', somos, name = 'somos'),
 ]
