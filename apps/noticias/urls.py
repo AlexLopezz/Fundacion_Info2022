48895d81d7259211ee7c8532492f5dd02f04b0c1
Alex Lopez <alexxxd23@gmail.com>
2022-08-21 00:20:43 -0300

    Agrege seccion de comentarios.

--- apps/noticias/urls.py
+++ apps/noticias/urls.py
@@ -3,8 +3,8 @@ from .views import noticias, articulo, categoria, mision
 
 
 urlpatterns = [
-    path('noticias/', noticias.as_view(), name='noticias'),
-    path('articulo/<int:pk>',articulo.as_view(), name= 'articulo'),
+    path('noticias/', noticias, name='noticias'),
+    path('articulo/<int:art>',articulo, name= 'articulo'),
     path('mision/', mision, name= 'mision'),
     path('categoria/<int:cat>', categoria, name ='categoria'),
 ]
