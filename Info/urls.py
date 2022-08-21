fc9a7a4e725740f5d64ccf519a9d69f806e64332
Alex Lopez <alexxxd23@gmail.com>
2022-08-20 21:42:40 -0300

    arregle el home y algunas cositas que estaban erroneas

--- Info/urls.py
+++ Info/urls.py
@@ -15,15 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import Home, mision
+from .views import home, mision, somos
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Home, name='home'),
+    path('', home, name='home'),
     path('accounts/', include('apps.usuarios.urls')), 
     path('accounts/', include('django.contrib.auth.urls')),
     path('seccion/',include('apps.noticias.urls')),
     path('mision/', mision, name = 'mision'),
+    path('somos/', somos, name = 'somos'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
