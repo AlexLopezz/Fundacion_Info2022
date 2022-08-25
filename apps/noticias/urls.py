1dfa5359a893c0e004dd2c0c8871e47b238a2f51
minocristian1 <cristiann_am97@hotmail.com>
2022-08-25 14:34:15 -0300

    asd

--- apps/noticias/urls.py
+++ apps/noticias/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
-<<<<<<< HEAD
-from .views import noticias, articulo, categoria, mision, buscarNoticias
-=======
-from .views import noticias, articulo, categoria, sobre_nosotros
->>>>>>> 3d45279f2603d896b495b4a8db1cefcb3ab0f4fd
+
+from .views import noticias, articulo, categoria, buscarNoticias
 
 
 urlpatterns = [
     path('noticias/', noticias, name='noticias'),
     path('articulo/<int:art>',articulo, name= 'articulo'),
-    path('sobre-nosotros/', sobre_nosotros, name= 'sobre nosotros'),
     path('categoria/<int:cat>', categoria, name ='categoria'),
     path('noticias/', buscarNoticias, name='buscarNoticias'),
 ]
