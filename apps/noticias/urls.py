be1bbf3cabb3e84bf85acfd02fe38fa106b633bd
Impagliazzo <julian.impagliazzo188@gmail.com>
2022-08-25 22:23:07 -0300

    actualizamos articulo

--- apps/noticias/urls.py
+++ apps/noticias/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import noticias
+from .views import noticias, articulo, categoria
+
 
 urlpatterns = [
     path('noticias/', noticias, name='noticias'),
+    path('articulo/<int:art>',articulo, name= 'articulo'),
+    path('categoria/<int:cat>', categoria, name ='categoria'),
 ]
