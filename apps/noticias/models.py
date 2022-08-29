de9435e8f7b3699d0bdc622add34ae493c3b7aa4
alex <alexxxd23@gmail.com>
2022-08-29 11:28:25 -0300

    asdsa

--- apps/noticias/models.py
+++ apps/noticias/models.py
@@ -38,10 +38,12 @@ class Modalidad(models.Model):
 
 class Evento(models.Model):
     nombre = models.CharField(max_length=150)
+    lugar = models.CharField(max_length=150)
     contenido = models.CharField(max_length= 200)
     fechaInicio = models.DateField('Fecha de inicio', auto_now = False, auto_now_add= True)
     fechaFin = models.DateField('Fecha de finalizacion', auto_now= False, auto_now_add = False)
     modalidad = models.ForeignKey(Modalidad, on_delete= models.CASCADE, null = True, blank = True)
+    gratuito = models.BooleanField(default = True, verbose_name= "Gratis")
     estado = models.BooleanField(default = True, verbose_name = 'Estado')
 
     def __str__(self):
